12f8ae6b95cb7d4e2da2b4088cf4252079f5c737
Jean-Francois Chevrette <jfchevrette@gmail.com>
2019-01-15 14:37:04 -0500

    move requirements out to requirements.txt

--- setup.py
+++ setup.py
@@ -11,10 +11,4 @@ setup(
     license='LICENSE.txt',
     description='Useful scripts for generating oncall reports from PagerDuty.',
     long_description=open('README.md').read(),
-    install_requires=[
-        "arrow >= 0.13.0",
-        "pypd >= 1.1.0",
-        "python-intervals >= 1.8.0",
-        "python-dateutil >= 2.7.5",
-    ],
 )
